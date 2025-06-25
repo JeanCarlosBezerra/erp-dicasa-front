65f11357d8c098fe60cd571007c88b18908500bd
Dicasa-GIT <git@dicasanet.com.br>
2025-06-25 17:55:13 -0300

    ajustes ip

--- vite.config.mjs
+++ vite.config.mjs
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://10.1.80.147:4200/',
+        target: 'http://172.28.7.5:4200/',
         changeOrigin: true,
         rewrite: path => path.replace(/^\/api/, '')
       }
