1c97bba4d580c633fe8b536ab1c09f6318ccda78
Dicasa-GIT <git@dicasanet.com.br>
2025-07-18 17:37:48 -0300

    Autenticacao via AD

---
+++ src/app/services/auth.interceptor.ts
@@ -0,0 +1,26 @@
+// src/app/services/auth.interceptor.ts
+import { Injectable } from '@angular/core';
+import {
+  HttpInterceptor,
+  HttpRequest,
+  HttpHandler,
+  HttpEvent
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const token = localStorage.getItem('access_token');
+    if (token) {
+      // cloneia a requisição e acrescenta o header
+      const authReq = req.clone({
+        setHeaders: {
+          Authorization: `Bearer ${token}`
+        }
+      });
+      return next.handle(authReq);
+    }
+    return next.handle(req);
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import 'zone.js';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideHttpClient } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
 import { HttpClientModule }    from '@angular/common/http';
@@ -10,15 +9,18 @@ import { routes } from './app/app.routes';
 import localePt from '@angular/common/locales/pt';
 import { LOCALE_ID } from '@angular/core';
 import { MAT_DATE_LOCALE } from '@angular/material/core';
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { AuthInterceptor } from './app/services/auth.interceptor';
 
 registerLocaleData(localePt);
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideHttpClient(),       // <- ESSENCIAL PARA O HttpClient funcionar
+    provideHttpClient(withInterceptorsFromDi()),       // <- ESSENCIAL PARA O HttpClient funcionar
     provideAnimations(),
     provideRouter(routes),
     { provide: LOCALE_ID,   useValue: 'pt-BR' },
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
     { provide: MAT_DATE_LOCALE, useValue: 'pt-BR' }    // Angular Material
   ]
 }).catch(err => console.error(err));
