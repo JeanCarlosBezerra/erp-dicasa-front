7e6b93d1d0aaf9055f5a759246e76c3952b57856
Dicasa-GIT <git@dicasanet.com.br>
2025-07-25 10:35:49 -0300

    Permissões

--- vite.config.mjs
+++ vite.config.mjs
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://172.28.7.5:4200/',
+        target: 'http://172.28.7.5:3000/',
         changeOrigin: true,
         rewrite: path => path.replace(/^\/api/, '')
       }
