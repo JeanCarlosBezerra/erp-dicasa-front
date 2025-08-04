00721b2466a6f809e3feedad04eaf8f4f1336943
Dicasa-GIT <git@dicasanet.com.br>
2025-08-04 15:52:12 -0300

    exportar

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -5,7 +5,7 @@ import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
-import { AuthService } from '@app/services/auth.service';
+import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 
