0c272b4662f4df019202b3c7bb63bfa6b285b5f1
Dicasa-GIT <git@dicasanet.com.br>
2025-08-04 16:06:58 -0300

    exportar

--- src/app/pages/comercial/colaborador/colaborador.component.ts
+++ src/app/pages/comercial/colaborador/colaborador.component.ts
@@ -55,8 +55,7 @@ export class ColaboradorComponent implements OnInit {
     'faturamento',
     'lucro',
     'margem',
-    'devolucoes',
-    'acoes'
+    'devolucoes'
   ];
 
   dataInicio!    : Date;
