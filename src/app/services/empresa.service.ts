0e8c86d1876c56901cd911dd262e539aa9a7ef11
Dicasa-GIT <git@dicasanet.com.br>
2025-08-08 17:00:11 -0300

    multiselecao empresas

--- src/app/services/empresa.service.ts
+++ src/app/services/empresa.service.ts
@@ -2,12 +2,13 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Empresa } from '../models/empresa.model';
+import { environment } from '../../environments/environment'; // ← importa aqui
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmpresaService {
-  private readonly url = '/empresas';
+  private readonly url = `${environment.apiUrl}/empresas`; // ← usa o IP fixo do environment.ts
 
   constructor(private http: HttpClient) {}
 
