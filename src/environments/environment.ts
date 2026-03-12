ecfa734d580b73fe9c71e65ea036267ba439fc02
JeanCarlosBezerra <cjeanbezerra@gmail.com>
2026-03-12 18:33:21 -0300

    ajuste

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'API_URL=http://172.28.7.5:3000', // se o Nest rodar na 3000
+  apiUrl: 'http://172.28.7.5:3000', // se o Nest rodar na 3000
 };
