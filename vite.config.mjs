a9047e4ea580c559c64f911ceace50129e21807a
JeanCarlosBezerra <cjeanbezerra@gmail.com>
2026-03-14 15:56:35 -0300

    config externo

--- vite.config.mjs
+++ vite.config.mjs
@@ -6,10 +6,11 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://172.28.7.5:3000/',
+        target: 'http://172.28.7.5:3000/',  // O endereço do seu backend
         changeOrigin: true,
-        rewrite: path => path.replace(/^\/api/, '')
-      }
-    }
-  }
+        rewrite: path => path.replace(/^\/api/, ''),
+      },
+    },
+    allowedHosts: ['erp.dicasaweb.com.br'],  // Adiciona seu domínio aqui
+  },
 });
