595682d247132d13e04f28648d5733953a3741db
JeanCarlosBezerra <cjeanbezerra@gmail.com>
2026-03-14 16:30:42 -0300

    config externo

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,15 @@
 import { defineConfig } from 'vite';
-import angular from '@analogjs/vite-plugin-angular';
 
 export default defineConfig({
-  plugins: [angular()],
   server: {
     proxy: {
       '/api': {
-        target: 'http://172.28.7.5:3000/',  // O endereço do seu backend
+        target: 'http://172.28.7.5:3000/', // O endereço do seu backend
         changeOrigin: true,
         rewrite: path => path.replace(/^\/api/, ''),
       },
     },
-    allowedHosts: ['erp.dicasaweb.com.br'],  // Adiciona seu domínio aqui
+    allowedHosts: ['erp.dicasaweb.com.br'], // Adiciona seu domínio aqui
   },
 });
+
