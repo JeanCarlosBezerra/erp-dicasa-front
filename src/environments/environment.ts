0b7332d0e3e24ffca9a6ebc1473c8cf1720a7c48
JeanCarlosBezerra <cjeanbezerra@gmail.com>
2026-03-14 18:02:13 -0300

    config externo

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  production: false,
-  apiUrl: 'https://172.28.7.5:3000', // se o Nest rodar na 3000
+  production: true,
+  apiUrl: 'https://api-erp.dicasaweb.com.br/', // trocar pelo domínio que seu amigo criar
 };
